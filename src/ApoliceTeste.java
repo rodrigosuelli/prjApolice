928344bdcd81e3746c3a7451fcf8322c49928dd4
Aluno <aluno@fatecmm.local>
2022-08-25 09:28:22 -0300

    First commit

---
+++ src/ApoliceTeste.java
@@ -0,0 +1,9 @@
+public class ApoliceTeste {
+    public static void main(String[] args) {
+        Apolice apolice = new Apolice();
+        apolice.setNomeSegurado("Joao Barbosa");
+        apolice.setIdade(45);
+        apolice.setValorPremio(1500.0f);
+        apolice.imprimir();
+    }
+}
